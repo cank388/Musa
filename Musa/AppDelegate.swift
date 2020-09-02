d9c3d02c04d565080b8e1c70faa2d30dd4186a10
Can <can@basefy.com>
2020-09-02 14:43:53 +0300

    Initial Commit

---
+++ Musa/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Musa
+//
+//  Created by Can Kalender on 2.09.2020.
+//  Copyright © 2020 Can Kalender. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Musa/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Musa
+//
+//  Created by Can Kalender on 2.09.2020.
+//  Copyright © 2020 Can Kalender. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
